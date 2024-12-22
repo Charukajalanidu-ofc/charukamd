3b9a405e23f09997ebe6d0683ffbc34373e66377
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-12-22 17:18:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'YOUR SESSION ID',
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (gph_without)' : process.env.GITHUB_AUTH_TOKEN,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
+SESSION_ID: 'HANSAMAL-MD=zBknWaBD#T3QMfKde3MhGqvuO-oPBjXzUrBNoy94SYg8-Yja_zbo',
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'pat_11BEXMDNA0vAVKYQXfREPc_6fNxfCUcfAwJAbNaqy5lrSPwhzGnGw1y1YmNsKVAFsrRASWDASYnndxegvS' : process.env.GITHUB_AUTH_TOKEN,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'charukajalanidu-ofc' : process.env.GITHUB_USER_NAME,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94775228949' : process.env.OWNER_NUMBER
 };
